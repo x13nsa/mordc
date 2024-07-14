58a8383ad08ec09ba50c359f31e31c2d9d9b6b2f
x13nsa <pmzx13nsa@gmail.com>
2024-07-14 15:21:46 -0500

    printing the message

--- main.s
+++ main.s
@@ -1,12 +1,19 @@
 .section	.text
 .globl		_start
 
-
-.macro	_PRINT, a, b, c
+.macro	_SHMSG, a, b
 	movq	$1, %rax
-	movq	\a, %rdi
-	leaq	\b, %rsi
-	movl	\c, %edx
+	movq	$1, %rdi
+	leaq	\a, %rsi
+	movl	\b, %edx
+	syscall
+.endm
+
+.macro	_PRINT, a, b
+	movq	$1, %rax
+	movq	$1, %rdi
+	movq	\a, %rsi
+	movl	\b, %edx
 	syscall
 .endm
 
@@ -15,17 +22,40 @@
 	movq	\a, %rdi
 	syscall
 .endm
-	
 
 _start:
 	# Getting number of arguments.
 	popq	%rax
 	cmpq	$3, %rax
 	jne	.print_usage
-
-
-	_EXIT	$0
+	# 1. executable name
+	# 2. mode.
+	# 3. message.
+	popq	%rax
+	popq	%rax
+	popq	%r15
+	movzbl	(%rax), %eax
+	cmpl	$'e', %eax
+	je	.encode_mode
+	cmpl	$'d', %eax
+	je	.decode_mode
 
 .print_usage:
-	_PRINT	$1, usage_m(%rip), usage_l(%rip)
+	_SHMSG	usage_m(%rip), usage_l(%rip)
 	_EXIT	$1
+
+.encode_mode:
+	movzbl	(%r15), %eax
+	testl	%eax, %eax	
+	jz	.byebye
+
+	_PRINT	%r15, $1
+
+	incq	%r15
+	jmp	.encode_mode
+
+.decode_mode:
+	_EXIT	$69
+
+.byebye:
+	_EXIT	$0
